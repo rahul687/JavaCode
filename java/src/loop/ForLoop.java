c4a64598e8ab7710267271f6a3bb685ed078ba14
rahul687 <rahulverma8087@gmail.com>
2018-04-30 22:46:51 -0500

    Java loop codes

---
+++ java/src/loop/ForLoop.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class ForLoop {
+    public static void main (String args[])
+    {
+        String array[]= {"Rahul","Ron","Raju"};
+
+        // Enhanced for loop
+        for (String x :array)
+        {
+            System.out.println(x);
+        }
+
+        // For loop begins
+        int x=2;
+        for (x=2;x<=9;x++)
+        System.out.println("Value of x:"+ x);
+    }
+}
+
+// www.geeksforgeeks.org
---
+++ java/src/loop/While.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class While {
+    public static void main (String args [])
+    {
+        int a=1;
+        // Exit when a is greater than 8
+
+        while (a<=8)
+        {
+            System.out.println("Value of a:" +a);
+            // increment of value
+            a++;
+        }
+
+    }
+}
